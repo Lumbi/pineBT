391ddee9ba815197bea2abfb8c1f2d524fdd9dcc
Gabriel <Lumbi@users.noreply.github.com>
2023-06-22 21:42:09 -0400

    Support range-based iterator on BT

--- pineBT/BehaviorTree.cpp
+++ pineBT/BehaviorTree.cpp
@@ -38,3 +38,55 @@ void BehaviorTree::print()
 {
 	BehaviorTreePrinter(*this).print();
 }
+
+// Iterator support
+
+BehaviorTree::iterator::reference BehaviorTree::iterator::operator*() const
+{
+	return *pointers.front();
+}
+
+BehaviorTree::iterator::pointer BehaviorTree::iterator::operator->()
+{
+	return pointers.front();
+}
+
+BehaviorTree::iterator& BehaviorTree::iterator::operator++()
+{
+	if (pointers.empty()) return *this;
+
+	pointer current = pointers.front();
+	pointers.pop();
+
+	// TODO: Maybe all behaviors should have a child array
+
+	if (auto composite = dynamic_cast<Composite*>(current); composite)
+	{
+		for (auto&& child : composite->getChildren())
+			pointers.push(child);
+	}
+	else if (auto decorator = dynamic_cast<Decorator*>(current); decorator)
+	{
+		if (auto child = decorator->getChild(); child)
+			pointers.push(child);
+	}
+
+	return *this;
+}
+
+BehaviorTree::iterator BehaviorTree::iterator::operator++(int)
+{
+	iterator old = *this; ++(*this); return old;
+}
+
+bool pineBT::operator==(const BehaviorTree::iterator& lhs, const BehaviorTree::iterator& rhs)
+{
+	if (lhs.pointers.empty() && rhs.pointers.empty()) return true;
+	if (lhs.pointers.empty() || rhs.pointers.empty()) return false;
+	return lhs.pointers.front() == rhs.pointers.front();
+}
+
+bool pineBT::operator!=(const BehaviorTree::iterator& lhs, const BehaviorTree::iterator& rhs)
+{
+	return !(lhs == rhs);
+}
--- pineBT/BehaviorTree.h
+++ pineBT/BehaviorTree.h
@@ -4,6 +4,8 @@
 #include "Builder/BehaviorTreeBuilder.h"
 
 #include <memory>
+#include <iterator>
+#include <queue>
 
 namespace pineBT
 {
@@ -34,5 +36,37 @@ namespace pineBT
 	private:
 		Behavior* root;
 		std::vector<std::unique_ptr<Behavior>> behaviors;
+
+
+		// Iterator support
+
+	public:
+		struct iterator
+		{
+			using iterator_category = std::forward_iterator_tag;
+			using value_type = Behavior;
+			using pointer = value_type*;
+			using reference = value_type&;
+
+			iterator(pointer start) : pointers({ start }) {}
+			iterator() {}
+
+			reference operator*() const;
+			pointer operator->();
+
+			iterator& operator++();
+			iterator operator++(int);
+
+			friend bool operator== (const iterator& lhs, const iterator& rhs);
+			friend bool operator!= (const iterator& lhs, const iterator& rhs);
+
+		private:
+			std::queue<pointer> pointers;
+		};
+
+		iterator begin() { return iterator(root); };
+		iterator end() { return iterator(); };
+		iterator begin() const { return iterator(root); };
+		iterator end() const { return iterator(); };
 	};
 }
