161d54101b4ca44812043869ba97aaa93c84b24e
Gabriel <Lumbi@users.noreply.github.com>
2023-06-28 10:22:27 -0400

    Setup CSS bundling

--- pineBTeditor/webpack.config.js
+++ pineBTeditor/webpack.config.js
@@ -1,10 +1,9 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
-
-const stylesHandler = "style-loader";
 
 const config = {
   entry: "./src/renderer.js",
@@ -18,8 +17,9 @@ const config = {
     host: "localhost",
   },
   plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
   ],
   module: {
     rules: [
@@ -29,7 +29,7 @@ const config = {
       },
       {
         test: /\.less$/i,
-        use: [stylesHandler, "css-loader", "less-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
