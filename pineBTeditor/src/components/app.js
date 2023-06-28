b70a8a96596f27cbbc6378ea4265cc4a9ccdc2d7
Gabriel <Lumbi@users.noreply.github.com>
2023-06-28 10:24:26 -0400

    Setup some basic canvas with dragging

--- pineBTeditor/src/components/app.js
+++ pineBTeditor/src/components/app.js
@@ -1,5 +1,86 @@
+import { useState, useRef, useEffect } from 'react'
+
+import './app.less'
+
+const testBehaviors = [
+    {
+        schema: 'Root',
+        id: 1,
+        position: { x: 0, y: 0 }
+    },
+    {
+        schema: 'Parallel',
+        id: 2,
+        position: { x: 0, y: 200 }
+    }
+]
+
 export default function App() {
+    const [behaviors, setBehaviors] = useState(testBehaviors)
+    const [scrollOffset, setScrollOffset] = useState({ x: 0, y: 0 })
+
+    const [isDragging, setDragging] = useState(false)
+
+    const behaviorViewport = useRef()
+
+    useEffect(() => {
+      if (behaviorViewport.current) {
+        behaviorViewport.current.style.transform = `translate(${scrollOffset.x}px, ${scrollOffset.y}px)`
+      }
+    }, [scrollOffset])
+
+    function handleCanvasOnMouseDown(event) {
+        event.preventDefault()
+        setDragging(true)
+    }
+
+    function handleCanvasOnMouseMove(event) {
+        if (isDragging) {
+            event.preventDefault()
+            setScrollOffset({
+                x: scrollOffset.x + event.movementX,
+                y: scrollOffset.y + event.movementY
+            })
+        }
+    }
+
+    function handleCanvasOnMouseUp(event) {
+        event.preventDefault()
+        setDragging(false)
+    }
+
     return <>
-        <h1>Hello world</h1>
+        <div 
+            className='behavior-canvas'
+            onMouseDown={handleCanvasOnMouseDown}
+            onMouseMove={handleCanvasOnMouseMove}
+            onMouseUp={handleCanvasOnMouseUp}
+        >
+            <div 
+                className='behavior-viewport'
+                ref={behaviorViewport}
+            >
+            {
+                behaviors.map(behavior =>
+                    <div 
+                        key={behavior.id}
+                        className='behavior-card'
+                        style={
+                            {
+                                position: 'relative',
+                                left: behavior.position.x,
+                                top: behavior.position.y,
+                                width: 300,
+                                height: 100,
+                            }
+                        }
+                    >
+                        <p>{behavior.schema}</p>
+                        <div className='behavior-card__handle'></div>
+                    </div>
+                )
+            }
+            </div>
+        </div>
     </>
 }
--- pineBTeditor/src/renderer.js
+++ pineBTeditor/src/renderer.js
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './components/app'
 
+import './renderer.less'
+
 document.body.innerHTML = '<div id="app"></div>'
 const root = createRoot(document.getElementById('app'));
 root.render(<App />);
