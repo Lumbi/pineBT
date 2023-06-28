3329102be1f89d8e8c4ee9f0f2e5993d155a7bc7
Gabriel <Lumbi@users.noreply.github.com>
2023-06-28 14:38:13 -0400

    Update behavior card

--- pineBTeditor/src/components/behavior-card.js
+++ pineBTeditor/src/components/behavior-card.js
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
+import bem from '../bem'
 
 import './behavior-card.less'
 
@@ -6,12 +7,16 @@ export default function BehaviorCard(props) {
     const { 
         behavior,
         updateBehavior,
+        connections
     } = props
 
+    const self = useRef()
     const position = behavior.position;
     const [isDragging, setDragging] = useState(false)
+    const [lastMousePosition, setLastMousePosition] = useState({ x: 0, y: 0 })
 
-    const self = useRef()
+    const hasParent = !!connections.find(c => c.to === behavior.id)
+    const hasChildren = !!connections.find(c => c.from === behavior.id)
 
     useEffect(() => {
       if (self.current) {
@@ -19,23 +24,30 @@ export default function BehaviorCard(props) {
       }
     }, [position])
 
+    function updateLastMousePosition(event) {
+        setLastMousePosition({ x: event.screenX, y: event.screenY })
+    }
+
     function handleMouseDown(event) {
         event.preventDefault()
         event.stopPropagation()
         setDragging(true)
+        updateLastMousePosition(event)
     }
 
     function handleMouseMove(event) {
         if (isDragging) {
             event.preventDefault()
             event.stopPropagation()
+            const delta = {
+                x: event.screenX - lastMousePosition.x,
+                y: event.screenY - lastMousePosition.y 
+            }
             updateBehavior({
                 id: behavior.id,
-                position: {
-                    x: position.x + event.movementX,
-                    y: position.y + event.movementY
-                }
+                position: { x: position.x + delta.x, y: position.y + delta.y }
             })
+            updateLastMousePosition(event)
         }
     }
 
@@ -57,18 +69,31 @@ export default function BehaviorCard(props) {
     return (
         <div 
             ref={self}
-            className={`behavior-card ${isDragging ? 'behavior-card--grabbing' : ''}`}
+            className={bem('behavior-card', null, { grabbing: isDragging })}
         >
             <div 
-                className='behavior-card__background'
+                className={bem('behavior-card', 'background')}
                 onMouseDown={handleMouseDown}
                 onMouseMove={handleMouseMove}
                 onMouseUp={handleMouseUp}
                 onMouseLeave={handleMouseLeave}
             />
-            <div className='behavior-card__content'>
+            <div className={bem('behavior-card', 'content')}>
+                <div 
+                    className={bem('behavior-card', 'parent-handle', { connected: hasParent })}
+                    onClick={(event) => {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }}
+                    onMouseDown={(event) => event.stopPropagation()}
+                    onMouseMove={(event) => event.stopPropagation()}
+                />
                 <p>{behavior.schema}</p>
-                <div className='behavior-card__handle'></div>
+                <div 
+                    className={bem('behavior-card', 'children-handle', { connected: hasChildren })}
+                    onMouseDown={(event) => event.stopPropagation()}
+                    onMouseMove={(event) => event.stopPropagation()}
+                />
             </div>
         </div>
     )
