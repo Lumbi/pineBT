72246dbaa512a388ba8fd7cc7140a097468c62a4
Gabriel <Lumbi@users.noreply.github.com>
2023-07-06 08:30:43 -0400

    Implement BlackboardCondition

---
+++ pineBT/Behavior/Decorator/BlackboardCondition.cpp
@@ -0,0 +1,68 @@
+#include "BlackboardCondition.h"
+
+#include <assert.h>
+
+using namespace pineBT;
+
+static const char* OPT_KEY = "key";
+static const char* OPT_PREDICATE = "predicate";
+static const char* OPT_TARGET = "target";
+
+BehaviorSchema BlackboardCondition::schema = BehaviorSchema::inherit(
+	"BlackboardCondition",
+	Condition::schema,
+	BehaviorSchema::Options().build()
+		.number(OPT_KEY)
+		.enumeration(OPT_PREDICATE, 4)
+		.number(OPT_TARGET)
+	.end()
+);
+
+void BlackboardCondition::configure(const Option& option)
+{
+	if (option.key == OPT_KEY)
+	{
+		assert(option.value.tag == Option::Value::Type::NUMBER);
+		key = static_cast<Blackboard::Key>(option.value.asNumber);
+	}
+	else if (option.key == OPT_PREDICATE)
+	{
+		assert(option.value.tag == Option::Value::Type::CASE);
+		predicate = static_cast<Predicate>(option.value.asCase);
+	}
+	else if (option.key == OPT_TARGET)
+	{
+		assert(option.value.tag == Option::Value::Type::NUMBER);
+		target = option.value.asNumber;
+	}
+}
+
+bool BlackboardCondition::check()
+{
+	switch (predicate)
+	{
+	case Predicate::EXIST:
+		return blackboard.has(key);
+
+	case Predicate::EQUAL: 
+	{
+		float value;
+		blackboard.get(key, value);
+		return value == target;
+	}
+
+	case Predicate::LESS_THAN:
+	{
+		float value;
+		blackboard.get(key, value);
+		return value < target;
+	}
+
+	case Predicate::GREATER_THAN:
+	{
+		float value;
+		blackboard.get(key, value);
+		return value > target;
+	}
+	}
+}
---
+++ pineBT/Behavior/Decorator/BlackboardCondition.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Condition.h"
+#include "Blackboard/Blackboard.h"
+
+namespace pineBT
+{
+	class BlackboardCondition : public Condition
+	{
+	public:
+		static BehaviorSchema schema;
+
+		enum class Predicate
+		{
+			EXIST,
+			EQUAL,
+			LESS_THAN,
+			GREATER_THAN
+		};
+
+	public:
+		BlackboardCondition(
+			const Blackboard& blackboard,
+			Blackboard::Key key = 0,
+			Predicate predicate = Predicate::EXIST,
+			float target = 0.f
+		)
+			: Condition(),
+			  blackboard(blackboard),
+			  key(key),
+			  predicate(predicate),
+			  target(target)
+		{}
+
+		void set(Blackboard::Key key, Predicate predicate, float target) { 
+			this->key = key;
+			this->predicate = predicate;
+			this->target = target;
+		}
+
+		virtual void configure(const Option&) override;
+
+		virtual bool check() override;
+
+	private:
+		const Blackboard& blackboard;
+		Blackboard::Key key;
+		Predicate predicate;
+		float target;
+	};
+}
